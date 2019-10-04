b1cbcc219a968c8b6d309794d11fde1f09641d39
Duco <git@ducode.org>
2019-10-04 23:54:28 +0200

    Converted Vue components to TypeScript

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,36 +1,59 @@
 <template>
   <v-app id="keep">
-    <v-app-bar app clipped-left color="amber">
+    <v-app-bar
+      app
+      clipped-left
+      color="amber"
+    >
       <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
       <span class="title ml-3 mr-5">
         Wolk&nbsp;
         <span class="font-weight-light">Notes</span>
       </span>
-      <v-text-field solo-inverted flat hide-details label="Search" prepend-inner-icon="mdi-magnify"></v-text-field>
+      <v-text-field
+        solo-inverted
+        flat
+        hide-details
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
       <div class="flex-grow-1"></div>
     </v-app-bar>
 
-    <v-navigation-drawer v-model="drawer" app clipped color="grey lighten-4">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+      color="grey lighten-4"
+    >
       <NotebookBar></NotebookBar>
     </v-navigation-drawer>
 
     <v-content>
-      <v-container fluid class="lighten-4">
+      <v-container
+        fluid
+        class="lighten-4"
+      >
         <router-view></router-view>
       </v-container>
     </v-content>
   </v-app>
 </template>
 
-<script>
-import NotebookBar from '@/components/NotebookBar';
-export default {
+<script lang="ts">
+import { mapState } from "vuex";
+import { Component, Vue } from "vue-property-decorator";
+import NotebookBar from "@/components/NotebookBar";
 
-  components: {
-      NotebookBar
-  },
-  data: () => ({
-    drawer: null
-  })
-};
+@Component({
+  components: { NotebookBar },
+  computed: mapState(["notebooks"])
+})
+export default class App extends Vue {
+  data() {
+    return {
+      drawer: null
+    };
+  }
+}
 </script>
--- ui/src/components/NotebookBar.vue
+++ ui/src/components/NotebookBar.vue
@@ -1,16 +1,40 @@
 <template>
-  <div>{{notebooks}}</div>
+  <v-row>
+    <v-col>
+      <v-list-item
+        @click="notebookClick(notebook)"
+        v-for="notebook of notebooks"
+        v-bind:key="notebook.id"
+      >
+        <v-list-item-action>
+          <v-icon>mdi-notebook</v-icon>
+        </v-list-item-action>
+        <v-list-item-title class="grey--text">{{notebook.name}}</v-list-item-title>
+      </v-list-item>
+    </v-col>
+  </v-row>
 </template>
-<script>
+
+<script lang="ts">
 import { mapState } from "vuex";
-export default {
-    mounted() {
-        this.$store.dispatch('loadNotebooks');
-    },
-    computed: mapState([
-        'notebooks'
-    ])
-};
+import { Component, Vue } from "vue-property-decorator";
+import Notebook from "../models/api/notebook";
+
+@Component({
+  components: {},
+  computed: mapState(["notebooks"])
+})
+export default class NotebookBar extends Vue {
+  constructor() {
+    super();
+  }
+
+  mounted() {
+    this.$store.dispatch("loadNotebooks");
+  }
+
+  notebookClick(notebook: Notebook) {}
+}
 </script>
 
 <style scoped>
