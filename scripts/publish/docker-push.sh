fb7bb35bb8d5b1027ea723357d5b2387bda7ef49
Duco <git@ducode.org>
2019-10-27 18:58:24 +0100

    Docker fix

--- scripts/publish/docker-push.sh
+++ scripts/publish/docker-push.sh
@@ -2,6 +2,10 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 ROOT_FOLDER="$DIR/../.."
 VERSION="$(cat $ROOT_FOLDER/version.txt)"
+REPO_NAME="dukeofharen/wolk"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker push dukeofharen/wolk:$VERSION
+docker build -t $REPO_NAME:$VERSION
+docker tag $REPO_NAME:$VERSION $REPO_NAME:latest
+docker push $REPO_NAME:$VERSION
+docker push $REPO_NAME:latest
