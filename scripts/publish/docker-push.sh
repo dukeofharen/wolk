0580eb1d0547ec2d99385d5f6eabfa9733a8de47
Duco <git@ducode.org>
2019-10-27 19:04:04 +0100

    Fixed error

--- scripts/publish/docker-push.sh
+++ scripts/publish/docker-push.sh
@@ -5,7 +5,7 @@ VERSION="$(cat $ROOT_FOLDER/version.txt)"
 REPO_NAME="dukeofharen/wolk"
 
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $REPO_NAME:$VERSION
+docker build -t $REPO_NAME:$VERSION .
 docker tag $REPO_NAME:$VERSION $REPO_NAME:latest
 docker push $REPO_NAME:$VERSION
 docker push $REPO_NAME:latest
