589a6e2ed027f4167b1e7867c9f054d78e4cc651
Duco <git@ducode.org>
2019-10-28 22:56:50 +0100

    Order notebooks by name alphabetically

--- ui/src/store/mutations/notebooks.ts
+++ ui/src/store/mutations/notebooks.ts
@@ -2,9 +2,14 @@ import { StateModel } from '@/models/store/stateModel';
 import Notebook from '@/models/api/notebook';
 
 export function SET_NOTEBOOKS(state: StateModel, notebooks: Notebook[]) {
+    notebooks.sort((a: Notebook, b: Notebook) => {
+        let textA = a.name.toUpperCase();
+        let textB = b.name.toUpperCase();
+        return (textA < textB) ? -1 : (textA > textB) ? 1 : 0;
+    });
     state.notebooks = notebooks;
-};
+}
 
 export function SET_CURRENT_NOTEBOOK(state: StateModel, notebook: Notebook) {
     state.currentNotebook = notebook;
-};
+}
