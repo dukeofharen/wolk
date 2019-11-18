32022b565dc6cab75b22566288f33f0eb8002afc
Duco <git@ducode.org>
2019-11-18 22:31:01 +0100

    Added ScrollToTop component

--- ui/src/App.vue
+++ ui/src/App.vue
@@ -1,78 +1,80 @@
 <template>
-  <v-app id="keep">
-    <v-app-bar
-      app
-      clipped-left
-      color="blue lighten-1"
-    >
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-      <span class="title ml-3 mr-5">
+    <v-app id="keep">
+        <v-app-bar
+                app
+                clipped-left
+                color="blue lighten-1"
+        >
+            <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+            <span class="title ml-3 mr-5">
         Wolk&nbsp;
         <span class="font-weight-light">Notes</span>
       </span>
-      <v-text-field
-        solo-inverted
-        flat
-        hide-details
-        label="Search"
-        prepend-inner-icon="mdi-magnify"
-        v-model="searchTerm"
-        v-debounce:200ms="searchTermChanged"
-        v-if="isSignedIn"
-      ></v-text-field>
-      <div class="flex-grow-1"></div>
-    </v-app-bar>
+            <v-text-field
+                    solo-inverted
+                    flat
+                    hide-details
+                    label="Search"
+                    prepend-inner-icon="mdi-magnify"
+                    v-model="searchTerm"
+                    v-debounce:200ms="searchTermChanged"
+                    v-if="isSignedIn"
+            ></v-text-field>
+            <div class="flex-grow-1"></div>
+        </v-app-bar>
 
-    <v-navigation-drawer
-      v-model="drawer"
-      app
-      clipped
-      color="grey lighten-4"
-    >
-      <NotebookBar></NotebookBar>
-    </v-navigation-drawer>
+        <v-navigation-drawer
+                v-model="drawer"
+                app
+                clipped
+                color="grey lighten-4"
+        >
+            <NotebookBar></NotebookBar>
+        </v-navigation-drawer>
 
-    <v-content>
-      <v-container
-        fluid
-        class="lighten-4"
-      >
-        <router-view></router-view>
-      </v-container>
-    </v-content>
-    <ToastMessages></ToastMessages>
-  </v-app>
+        <v-content>
+            <v-container
+                    fluid
+                    class="lighten-4"
+            >
+                <router-view></router-view>
+            </v-container>
+        </v-content>
+        <ToastMessages></ToastMessages>
+        <ScrollToTop></ScrollToTop>
+    </v-app>
 </template>
 
 <script lang="ts">
-import { mapState } from "vuex";
-import { Component, Vue } from "vue-property-decorator";
-import NotebookBar from "@/components/NotebookBar.vue";
-import ToastMessages from "@/components/ToastMessages.vue";
+    import {mapState} from "vuex";
+    import {Component, Vue} from "vue-property-decorator";
+    import NotebookBar from "@/components/NotebookBar.vue";
+    import ToastMessages from "@/components/ToastMessages.vue";
+    import ScrollToTop from "@/components/ScrollToTop.vue";
 
-@Component({
-  components: { NotebookBar, ToastMessages },
-  computed: mapState(["notebooks"])
-})
-export default class App extends Vue {
-  drawer: boolean = true;
-  searchTerm: string = "";
+    @Component({
+        components: {NotebookBar, ToastMessages, ScrollToTop},
+        computed: mapState(["notebooks"])
+    })
+    export default class App extends Vue {
+        drawer: boolean = true;
+        searchTerm: string = "";
 
-  get isSignedIn(): boolean {
-    return this.$store.getters.isSignedIn;
-  }
+        get isSignedIn(): boolean {
+            return this.$store.getters.isSignedIn;
+        }
 
-  searchTermChanged() {
-    if (this.searchTerm) {
-      this.$router.push({
-        name: "overview",
-        query: <any>{ searchTerm: this.searchTerm }
-      });
-    } else {
-      this.$router.push({
-        name: "overview"
-      });
+        searchTermChanged() {
+            if (this.searchTerm) {
+                this.$router.push({
+                    name: "overview",
+                    query: <any>{searchTerm: this.searchTerm}
+                });
+            } else {
+                this.$router.push({
+                    name: "overview"
+                });
+            }
+        }
     }
-  }
-}
 </script>
