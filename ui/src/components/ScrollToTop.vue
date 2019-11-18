520b8e46373f49ebe59830e84a34452186c2b5dc
Duco <git@ducode.org>
2019-11-18 22:31:01 +0100

    Added ScrollToTop component

---
+++ ui/src/components/ScrollToTop.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-btn
+            fixed
+            dark
+            fab
+            bottom
+            right
+            color="pink"
+            title="Back to top"
+            @click="backToTop"
+    >
+        <v-icon>mdi-arrow-up</v-icon>
+    </v-btn>
+</template>
+
+<script lang="ts">
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class ScrollToTop extends Vue {
+        
+        get showButton(): boolean {
+            return document.body.scrollTop > 20 || document.documentElement.scrollTop > 20;
+        }
+        
+        backToTop() {
+            document.body.scrollTop = 0;
+            document.documentElement.scrollTop = 0;
+        }
+    }
+</script>
