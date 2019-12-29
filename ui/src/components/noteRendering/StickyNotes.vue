ab7acef9b90e4fc2ff5104423b684ac90ca4dde2
Duco <git@ducode.org>
2019-12-29 13:30:07 +0100

    Show sticky notes on screen

--- ui/src/components/noteRendering/StickyNotes.vue
+++ ui/src/components/noteRendering/StickyNotes.vue
@@ -1,10 +1,20 @@
 <template>
-    <div>{{contents}}</div>
+    <v-container class="lighten-5">
+        <v-row no-gutters>
+            <v-col cols="12" sm="4" v-for="model of models" :key="model.title">
+                <v-card class="pa-2 sticky-note" tile>
+                    <v-card-title>{{model.title}}</v-card-title>
+                    <v-card-text>{{model.contents}}</v-card-text>
+                </v-card>
+            </v-col>
+        </v-row>
+    </v-container>
 </template>
 
 <script lang="ts">
-    import {Component, Vue, Watch, Prop} from "vue-property-decorator";
+    import {Component, Vue, Prop} from "vue-property-decorator";
     import {stickyNotesToModel} from "@/services/stickyNoteService";
+    import {StickyNotesModel} from "@/models/stickyNotesModel";
 
     @Component({
         components: {}
@@ -12,12 +22,17 @@
     export default class StickyNotes extends Vue {
         @Prop()
         contents!: string;
-        
+
+        models: StickyNotesModel[] = [];
+
         mounted() {
-            console.log(stickyNotesToModel(this.contents));
+            this.models = stickyNotesToModel(this.contents);
         }
     }
 </script>
 
 <style scoped>
+    .sticky-note {
+        margin: 10px;
+    }
 </style>
