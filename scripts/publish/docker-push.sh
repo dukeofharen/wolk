37ef37bdc7a60a636cb3c4491cf76ea135d305ca
Duco <git@ducode.org>
2020-01-02 21:58:12 +0100

    Only deploy Travis artifacts on master

--- scripts/publish/docker-push.sh
+++ scripts/publish/docker-push.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-ROOT_FOLDER="$DIR/../.."
-VERSION="$(cat $ROOT_FOLDER/version.txt)"
-REPO_NAME="dukeofharen/wolk"
-
-echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
-docker build -t $REPO_NAME:$VERSION .
-docker tag $REPO_NAME:$VERSION $REPO_NAME:latest
-docker push $REPO_NAME:$VERSION
-docker push $REPO_NAME:latest
+if [ "$TRAVIS_BRANCH" = "master" ]; then
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+    ROOT_FOLDER="$DIR/../.."
+    VERSION="$(cat $ROOT_FOLDER/version.txt)"
+    REPO_NAME="dukeofharen/wolk"
+    
+    echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+    docker build -t $REPO_NAME:$VERSION .
+    docker tag $REPO_NAME:$VERSION $REPO_NAME:latest
+    docker push $REPO_NAME:$VERSION
+    docker push $REPO_NAME:latest
+fi
