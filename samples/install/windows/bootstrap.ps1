f6de124db8e364ee503ec24484c02659caed8144
Duco <git@ducode.org>
2020-01-06 21:45:32 +0100

    Deploy test backup to Wolk when deploying Windows Vagrant box

--- samples/install/windows/bootstrap.ps1
+++ samples/install/windows/bootstrap.ps1
@@ -27,7 +27,9 @@ function Add-Env-Variable($envVarsElement, $name, $value) {
 
     $envVarsElement.AppendChild($element)
 }
-$wolkEnvContent = Get-Content "C:\wolk\samples\install\env.json" -Raw
+
+$samplesRootPath = "C:\wolk\samples"
+$wolkEnvContent = Get-Content (Join-Path -Path $samplesRootPath "install\env.json") -Raw
 $wolkEnv = ConvertFrom-Json $wolkEnvContent
 $iisUser = "IIS_IUSRS"
 $tmpFolder = $env:TEMP
@@ -149,4 +151,16 @@ Write-Host "Adding site to IIS."
 New-WebSite -Name Wolk -Port 80 -PhysicalPath "$env:systemdrive\bin\wolk" -Force
 Start-Website -Name Wolk -ErrorAction SilentlyContinue
 
-# TODO upload test backup to Wolk
+# First, retrieve a JWT from Wolk
+Write-Host "Requesting Wolk JSON web token"
+$loginBody = ConvertTo-Json @{email = $wolkEnv.DefaultLoginEmail; password = $wolkEnv.DefaultPassword}
+$response = Invoke-WebRequest -Method Post -Uri "http://localhost/api/user/authenticate" -Body $loginBody -ContentType "application/json" -UseBasicParsing
+$authenticationJson = ConvertFrom-Json $response.Content
+$jwt = $authenticationJson.token
+
+# Upload the backup to Wolk
+Write-Host "Restoring backup for testing purposes"
+$wolkBackupPath = Join-Path -Path $samplesRootPath "install\wolk-backup-test.zip"
+$base64Backup = [Convert]::ToBase64String([IO.File]::ReadAllBytes($wolkBackupPath))
+$backupBody = ConvertTo-Json @{zipBytes = $base64Backup}
+Invoke-WebRequest -Method Post -Uri "http://localhost/api/backup" -Body $backupBody -ContentType "application/json" -Headers @{"Authorization" = "Bearer $jwt"} -UseBasicParsing
