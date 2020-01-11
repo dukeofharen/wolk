70829271ce6e1a7e20085a666b15862cfc0dbcb1
Duco <git@ducode.org>
2020-01-11 11:03:06 +0100

    Fixed Vagrant Docker box

--- samples/install/linux-with-docker/bootstrap.sh
+++ samples/install/linux-with-docker/bootstrap.sh
@@ -15,81 +15,84 @@ snap install jq
 . $DIR/install-docker.sh
 . $DIR/install-docker-compose.sh
 
-# # Stop Wolk service
-# systemctl stop wolk
+# Stop Wolk service
+systemctl stop wolk
 
-# # Extract Wolk
-# INSTALL_PATH="/opt/wolk"
-# if [ -d "$INSTALL_PATH" ]; then
-#     echo "Deleting $INSTALL_PATH"
-#     rm -r "$INSTALL_PATH"
-# fi
+# Creating data directories
+export WOLK_ROOT="/srv/wolk"
+if [ ! -d "$WOLK_ROOT" ]; then
+    echo "Creating $WOLK_ROOT"
+    mkdir "$WOLK_ROOT"
+fi
 
-# echo "Creating $INSTALL_PATH"
-# mkdir "$INSTALL_PATH"
+export WOLK_UPLOADS_ROOT="$WOLK_ROOT/uploads"
+if [ ! -d "$WOLK_UPLOADS_ROOT" ]; then
+    echo "Creating $WOLK_UPLOADS_ROOT"
+    mkdir "$WOLK_UPLOADS_ROOT"
+fi
 
-# tar -xvzf "$DOWNLOAD_PATH" -C "$INSTALL_PATH"
+# Install Wolk files
+INSTALL_PATH="/opt/wolk"
+if [ -d "$INSTALL_PATH" ]; then
+    echo "Deleting $INSTALL_PATH"
+    rm -r "$INSTALL_PATH"
+fi
 
-# # Creating data directories
-# WOLK_ROOT="/srv/wolk"
-# if [ ! -d "$WOLK_ROOT" ]; then
-#     echo "Creating $WOLK_ROOT"
-#     mkdir "$WOLK_ROOT"
-# fi
+echo "Creating $INSTALL_PATH"
+mkdir "$INSTALL_PATH"
+cp "$DIR/run.sh" "$INSTALL_PATH"
 
-# export WOLK_UPLOADS_ROOT="$WOLK_ROOT/uploads"
-# if [ ! -d "$WOLK_UPLOADS_ROOT" ]; then
-#     echo "Creating $WOLK_UPLOADS_ROOT"
-#     mkdir "$WOLK_UPLOADS_ROOT"
-# fi
+export WOLKDIR="$INSTALL_PATH"
+export JWT_SECRET=$(echo "$ENV" | jq -r .JwtSecret)
+export JWT_EXPIRATION=$(echo "$ENV" | jq -r .ExpirationInSeconds)
+export DEFAULT_LOGIN_EMAIL=$(echo "$ENV" | jq -r .DefaultLoginEmail)
+export DEFAULT_PASSWORD=$(echo "$ENV" | jq -r .DefaultPassword)
+envsubst < $DIR/docker-compose.yml.template > "$INSTALL_PATH/docker-compose.yml"
 
-# # Install Wolk service
-# echo "Installing Wolk service"
-# export WOLKDIR="$INSTALL_PATH"
-# export DB_PATH="$WOLK_ROOT/wolk.db"
-# export JWT_SECRET=$(echo "$ENV" | jq -r .JwtSecret)
-# export JWT_EXPIRATION=$(echo "$ENV" | jq -r .ExpirationInSeconds)
-# export DEFAULT_LOGIN_EMAIL=$(echo "$ENV" | jq -r .DefaultLoginEmail)
-# export DEFAULT_PASSWORD=$(echo "$ENV" | jq -r .DefaultPassword)
+# Pulling Docker image
+docker pull dukeofharen/wolk:latest
 
-# envsubst < $DIR/wolk.service.template > /etc/systemd/system/wolk.service
-# systemctl enable wolk
-# systemctl daemon-reload
-# systemctl start wolk
+# Install Wolk service
+echo "Installing Wolk service"
 
-# # Install Wolk in Nginx
-# echo "Adding site for Wolk in Nginx"
-# LOCAL_NGINX_CONF_PATH="$DIR/wolk.conf"
-# NGINX_ROOT_PATH="/etc/nginx"
-# NGINX_AVAILABLE_PATH="$NGINX_ROOT_PATH/sites-available"
-# NGINX_ENABLED_PATH="$NGINX_ROOT_PATH/sites-enabled"
+envsubst < $DIR/wolk.service.template > /etc/systemd/system/wolk.service
+systemctl enable wolk
+systemctl daemon-reload
+systemctl start wolk
 
-# NGINX_DEFAULT_PATH="$NGINX_ENABLED_PATH/default"
-# if [ -f "$NGINX_DEFAULT_PATH" ]; then
-#     echo "Deleting $NGINX_DEFAULT_PATH"
-#     rm "$NGINX_DEFAULT_PATH"
-# fi
+# Install Wolk in Nginx
+echo "Adding site for Wolk in Nginx"
+LOCAL_NGINX_CONF_PATH="$DIR/wolk.conf"
+NGINX_ROOT_PATH="/etc/nginx"
+NGINX_AVAILABLE_PATH="$NGINX_ROOT_PATH/sites-available"
+NGINX_ENABLED_PATH="$NGINX_ROOT_PATH/sites-enabled"
 
-# cp "$LOCAL_NGINX_CONF_PATH" "$NGINX_AVAILABLE_PATH"
-# ln -s "$NGINX_AVAILABLE_PATH/wolk.conf" "$NGINX_ENABLED_PATH/wolk.conf"
-# systemctl restart nginx
-# echo "Sleeping for 5 seconds; waiting for Wolk and Nginx to initialize"
-# sleep 5
+NGINX_DEFAULT_PATH="$NGINX_ENABLED_PATH/default"
+if [ -f "$NGINX_DEFAULT_PATH" ]; then
+    echo "Deleting $NGINX_DEFAULT_PATH"
+    rm "$NGINX_DEFAULT_PATH"
+fi
 
-# # First, retrieve a JWT from Wolk
-# echo "Requesting Wolk JSON web token"
-# TOKEN_RESPONSE=$(curl -s \
-#     -d "{\"email\": \"$DEFAULT_LOGIN_EMAIL\", \"password\": \"$DEFAULT_PASSWORD\"}" \
-#     -H "Content-Type: application/json" \
-#     -X POST http://localhost/api/user/authenticate)
-# JWT=$(echo $TOKEN_RESPONSE | jq -r .token)
+cp "$LOCAL_NGINX_CONF_PATH" "$NGINX_AVAILABLE_PATH"
+ln -nsf "$NGINX_AVAILABLE_PATH/wolk.conf" "$NGINX_ENABLED_PATH/wolk.conf"
+systemctl restart nginx
+echo "Sleeping for 5 seconds; waiting for Wolk and Nginx to initialize"
+sleep 5
 
-# # Upload the backup to Wolk
-# echo "Restoring backup for testing purposes"
-# BASE64_BACKUP=$(cat $BACKUP_PATH | base64 -w0)
-# echo "{\"zipBytes\": \"$BASE64_BACKUP\"}" > backup-body.json
-# curl -s \
-#     -d @backup-body.json \
-#     -H "Content-Type: application/json" \
-#     -H "Authorization: Bearer $JWT" \
-#     -X POST http://localhost/api/backup
+# First, retrieve a JWT from Wolk
+echo "Requesting Wolk JSON web token"
+TOKEN_RESPONSE=$(curl -s \
+    -d "{\"email\": \"$DEFAULT_LOGIN_EMAIL\", \"password\": \"$DEFAULT_PASSWORD\"}" \
+    -H "Content-Type: application/json" \
+    -X POST http://localhost/api/user/authenticate)
+JWT=$(echo $TOKEN_RESPONSE | jq -r .token)
+
+# Upload the backup to Wolk
+echo "Restoring backup for testing purposes"
+BASE64_BACKUP=$(cat $BACKUP_PATH | base64 -w0)
+echo "{\"zipBytes\": \"$BASE64_BACKUP\"}" > backup-body.json
+curl -s \
+    -d @backup-body.json \
+    -H "Content-Type: application/json" \
+    -H "Authorization: Bearer $JWT" \
+    -X POST http://localhost/api/backup
--- samples/install/linux-with-docker/install-docker.sh
+++ samples/install/linux-with-docker/install-docker.sh
@@ -7,6 +7,6 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 apt-get update
-ensure_package docker-ce
-ensure_package docker-ce-cli
-ensure_package containerd.io
+apt install docker-ce -y
+apt install docker-ce-cli -y
+apt install containerd.io -y
