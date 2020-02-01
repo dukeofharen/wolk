4dbf57c8636efa0223d7f9cd3412d8ba60420fb4
Duke <github@ducode.org>
2020-02-01 13:39:33 +0000

    Bug/sort by last opened (#19)
    * Show todotxt form by default
    
    * Fixed ordering of notes by opened datetime

--- ui/src/store/mutations/notes.ts
+++ ui/src/store/mutations/notes.ts
@@ -2,21 +2,6 @@ import {StateModel} from '@/models/store/stateModel';
 import Note from '@/models/api/note';
 
 export function SET_NOTES(state: StateModel, notes: Note[]) {
-    notes.sort((a: Note, b: Note) => {
-        if (!a.opened) {
-            return 1;
-        }
-
-        if (a.opened && !b.opened) {
-            return 1;
-        }
-
-        if (b.opened && new Date(a.opened).getTime() < new Date(b.opened).getTime()) {
-            return 1;
-        }
-
-        return 0;
-    });
     state.notes = notes;
 }
 
