712eeb45b530f9c922b4600b1af8c468309cd7e1
Duco <git@ducode.org>
2020-02-02 18:03:58 +0100

    Added test for login incorrect

--- testsuite/cypress/integration/general/login_spec.js
+++ testsuite/cypress/integration/general/login_spec.js
@@ -11,4 +11,12 @@ describe('Login test', () => {
         cy.get('#login').click();
         cy.url().should('include', '/overview');
     });
+
+    it('Logging in with incorrect credentials should not redirect and show error', () => {
+        cy.get('input[name="email"]').type('wolk@example.com');
+        cy.get('input[name="password"]').type('WRONGPASS');
+        cy.get('#login').click();
+        cy.url().should('include', '/login');
+        cy.get('.v-snack__content').should('contain', 'The provided credentials are incorrect.');
+    });
 });
