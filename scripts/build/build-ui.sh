5034ac3dfabffcc5f20fed915c4a69906eee9fd3
Duco <git@ducode.org>
2020-03-15 20:53:11 +0100

    Build UI fix

--- scripts/build/build-ui.sh
+++ scripts/build/build-ui.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 # Building user interface
 echo "Building Vue.js user interface"
-GUI_DIST_FOLDER="ui/dist"
-GUI_DESTINATION_FOLDER="Ducode.Wolk.Api/gui"
-
 cd ui
 npm install
 if ! npm run build; then
     exit 1
 fi
-
-echo "Copying files from $GUI_DIST_FOLDER to $GUI_DESTINATION_FOLDER"
-cp -a "$GUI_DIST_FOLDER/." "$GUI_DESTINATION_FOLDER"
