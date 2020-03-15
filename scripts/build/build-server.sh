58fc9578ba3295fbd17aac704ed7b54124e732df
Duco <git@ducode.org>
2020-03-15 21:31:32 +0100

    Updated server build script

--- scripts/build/build-server.sh
+++ scripts/build/build-server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+ROOT_PATH="$DIR/../.."
 VERSION=$(cat version.txt)
-API_ROOT_PATH="$DIR/src/Ducode.Wolk.Api"
+API_ROOT_PATH="$ROOT_PATH/src/Ducode.Wolk.Api"
 
-mkdir "$DIR/dist"
+mkdir "$ROOT_PATH/dist"
 
 # Copy UI to project
 mkdir "$API_ROOT_PATH/gui"
-cp "$DIR/ui/dist/*" "$API_ROOT_PATH/gui"
+cp "$ROOT_PATH/ui/dist/*" "$API_ROOT_PATH/gui"
 
 # Patch .csproj files with new version.
 find src -name "*.csproj" | while read FILENAME; do
@@ -15,8 +16,8 @@ find src -name "*.csproj" | while read FILENAME; do
 done
 
 # Build for Windows
-WIN_BIN_DIR="$DIR/src/Ducode.Wolk.Api/bin/release/netcoreapp3.1/win-x64/publish"
-TAR_LOCATION="$DIR/dist/wolk_windows.tar.gz"
+WIN_BIN_DIR="$ROOT_PATH/src/Ducode.Wolk.Api/bin/release/netcoreapp3.1/win-x64/publish"
+TAR_LOCATION="$ROOT_PATH/dist/wolk_windows.tar.gz"
 
 echo "Building Wolk for Windows"
 if ! dotnet publish "$API_ROOT_PATH/Ducode.Wolk.Api.csproj" -c release --runtime=win-x64; then
@@ -26,11 +27,11 @@ fi
 echo "Zipping binaries for Windows"
 cd "$WIN_BIN_DIR"
 tar -czvf "$TAR_LOCATION" -C "$WIN_BIN_DIR" .
-cd "$DIR"
+cd "$ROOT_PATH"
 
 # Build for Linux
-LIN_BIN_DIR="$DIR/src/Ducode.Wolk.Api/bin/release/netcoreapp3.1/linux-x64/publish"
-TAR_LOCATION="$DIR/dist/wolk_linux.tar.gz"
+LIN_BIN_DIR="$ROOT_PATH/src/Ducode.Wolk.Api/bin/release/netcoreapp3.1/linux-x64/publish"
+TAR_LOCATION="$ROOT_PATH/dist/wolk_linux.tar.gz"
 
 echo "Building Wolk for Linux"
 if ! dotnet publish "$API_ROOT_PATH/Ducode.Wolk.Api.csproj" -c release --runtime=linux-x64; then
@@ -40,10 +41,10 @@ fi
 echo "Archiving binaries for Linux"
 cd "$LIN_BIN_DIR"
 tar -czvf "$TAR_LOCATION" -C "$LIN_BIN_DIR" .
-cd "$DIR"
+cd "$ROOT_PATH"
 
 # Creating Swagger file
-SWAGGERGEN_ROOT_PATH="$DIR/src/Ducode.Wolk.SwaggerGenerator"
+SWAGGERGEN_ROOT_PATH="$ROOT_PATH/src/Ducode.Wolk.SwaggerGenerator"
 SWAGGER_PATH="$SWAGGERGEN_ROOT_PATH/bin/Release/netcoreapp3.1/swagger.json"
 echo "Creating swagger.json file"
 if ! dotnet run -c Release --project "$SWAGGERGEN_ROOT_PATH"; then
@@ -51,4 +52,4 @@ if ! dotnet run -c Release --project "$SWAGGERGEN_ROOT_PATH"; then
 fi
 
 echo "swagger.json file created successfully!"
-cp "$SWAGGER_PATH" "$DIR/dist"
+cp "$SWAGGER_PATH" "$ROOT_PATH/dist"
