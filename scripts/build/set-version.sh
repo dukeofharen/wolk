8079a9cc0ecb2c5fc924590ecf0e0a8a8d537c0c
Duco <git@ducode.org>
2020-03-15 20:19:18 +0100

    Added build ID to version

--- scripts/build/set-version.sh
+++ scripts/build/set-version.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
+BUILD_ID="$1"
+
 echo "Determining version"
 YEAR="$(date +%Y)"
 MONTH="$(date +%m)"
 DAY="$(date +%d)"
-VERSION="$YEAR.$MONTH.$DAY.0"
+VERSION="$YEAR.$MONTH.$DAY.$BUILD_ID"
 echo "Version is $VERSION"
 echo "$VERSION" > version.txt
